beb36192bca22174837c6ffb052244a0d21a2e43
Puja <pujayadav@gmail.com>
2020-09-27 08:32:38 +0530

    working hours or days is reached for a month

---
+++ UC6/UC6/UC6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UC6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Wage_Per_Hour = 20;
+            var No_Of_Hour = 100;
+
+            var Modified_monthly_Wage = Wage_Per_Hour * No_Of_Hour;
+            Console.WriteLine("Modified Monthly Wage is {0}",Modified_monthly_Wage);
+        }
+    }
+}
