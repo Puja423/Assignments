fa1662c2446fd18c2f6194d5bcda24d98f4fd57b
Puja <pujayadav@gmail.com>
2020-09-27 08:23:07 +0530

    Wages For A Month

---
+++ UC5/UC5/UC5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Wage_Per_Hour = 20;
+            var Full_Day_Hour = 8;
+
+            var Daily_employee_wage = Wage_Per_Hour * Full_Day_Hour;
+            var Monthly_wage = Daily_employee_wage * Wage_Per_Hour;
+            Console.WriteLine("Monthly_wage is{0}",Monthly_wage);
+        }
+    }
+}
